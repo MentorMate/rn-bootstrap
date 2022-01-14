ebda9c74b7683e1d325e038740daf79b0fec5812
Ivan Ivanov <ivan.ivanov@mentormate.com>
2022-01-14 15:34:04 +0200

    Add React Navigation - Stack and Botttom Tabs

--- boilerplate/App.tsx
+++ boilerplate/App.tsx
@@ -1,13 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
+import 'react-native-gesture-handler';
 import React from 'react';
 import { SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
 
