3af0e1909fd1937d592b99b214d5993008ff723e
Ivan Ivanov <ivan.ivanov@mentormate.com>
2022-01-14 16:53:37 +0200

    Setup Jest

---
+++ boilerplate/jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  preset: 'jest-expo',
+  verbose: true,
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFiles: ['<rootDir>/config/jest/jestSetup.ts'],
+  moduleNameMapper: {
+    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'babel-jest',
+    '\\.(png|jpg|jpeg||gif|svg)$': '<rootDir>/config/jest/transformAssets.js'
+  },
+  watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname', 'jest-watch-select-projects'],
+  transformIgnorePatterns: ['/!node_modules\\/@b9/react-native-autocomplete-search/'],
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80
+    }
+  }
+};
