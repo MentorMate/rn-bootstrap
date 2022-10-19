90b53edd86d7190e9b68952707dfb0e6e8fc08b6
Biser Atanasov <biser.atanasov@mentormate.com>
2022-10-19 18:26:50 +0300

    chore: update RN to 70.3 and update other deps

--- src/tools/dependency-versions.ts
+++ src/tools/dependency-versions.ts
@@ -1,13 +1,13 @@
 export const navigationDeps = [
-  '@react-navigation/bottom-tabs@^6.3.2',
-  '@react-navigation/native@^6.0.11',
-  '@react-navigation/stack@^6.2.2',
-  "@react-navigation/elements@^1.3.4",
-  'react-native-safe-area-context@^4.2.4',
-  'react-native-screens@^3.15.0'
+  '@react-navigation/bottom-tabs@^6.4.0',
+  '@react-navigation/native@^6.0.13',
+  '@react-navigation/stack@^6.3.2',
+  "@react-navigation/elements@^1.3.6",
+  'react-native-safe-area-context@^4.4.1',
+  'react-native-screens@^3.18.2'
 ];
 
-export const reduxDeps = ['@reduxjs/toolkit@^1.8.3', 'react-redux@^8.0.2'];
+export const reduxDeps = ['@reduxjs/toolkit@^1.8.6', 'react-redux@^8.0.4'];
 
-export const styleDeps = ['styled-components@^5.3.3'];
+export const styleDeps = ['styled-components@^5.3.6'];
 export const styleDevDeps = ['@types/styled-components-react-native@^5.1.3'];
