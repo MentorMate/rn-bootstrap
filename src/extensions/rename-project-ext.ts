8dc331aa99611f23ca7358a6465df5b34240190c
Nikolay Donev <nikolay.donev@mentormate.com>
2023-10-10 13:45:39 +0300

    chore: update react-rename package after official maintainer update

--- src/extensions/rename-project-ext.ts
+++ src/extensions/rename-project-ext.ts
@@ -3,9 +3,7 @@ import { RnBootstrapToolbox } from '../types/RnBootstrapToolbox';
 
 module.exports = (toolbox: RnBootstrapToolbox) => {
   toolbox.renameProject = (projectName: string, bundleIdentifier: string) => {
-    // Using a forked and slightly altered version of react-native-rename due to the package not working with react-native 0.69:
-    // https://github.com/junedomingo/react-native-rename/issues/159
-    const renameCmd = `npx --yes @mmbiser/react-native-rename ${projectName} -b ${bundleIdentifier}`;
+    const renameCmd = `npx react-native-rename ${projectName} -b ${bundleIdentifier} --skipGitStatusCheck`;
     return spawnProgress(renameCmd);
   };
 };
