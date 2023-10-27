2bb731ce194eb938fdec321e05610fb17602e3e9
Nikolay Donev <nikolay.donev@mentormate.com>
2023-10-27 19:38:08 +0300

    revert please release

---
+++ scripts/getLatestVersion.js
@@ -0,0 +1,21 @@
+const { spawnSync } = require('child_process');
+
+function getLatestVersion(packageJson) {
+  let latestVersion;
+  const latestPublishedVersion = spawnSync('npm', [
+    'show',
+    packageJson.name,
+    'version'
+  ]);
+
+  if (latestPublishedVersion.stderr.toString()) {
+    // The package has never been published
+    latestVersion = packageJson.version;
+  } else {
+    latestVersion = latestPublishedVersion.stdout.toString().trim();
+  }
+
+  return latestVersion;
+}
+
+module.exports = { getLatestVersion };
---
+++ scripts/saveVersion.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const { gitAdd, gitCommit } = require('./simpleGit');
+
+function saveVersion(packageJson) {
+  const { version } = packageJson;
+
+  fs.writeFile(
+    './package.json',
+    Buffer.from(JSON.stringify(packageJson, null, 2)),
+    { encoding: 'utf8' },
+    err => {
+      if (err) {
+        return console.log('Error!', err);
+      }
+
+      gitAdd();
+      gitCommit(`chore: increment version: ${version}`);
+
+      return console.log(
+        `Successfully updated and committed package.json to version ${version}`
+      );
+    }
+  );
+}
+
+module.exports = { saveVersion };
