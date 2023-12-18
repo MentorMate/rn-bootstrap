3713eac777c4e8035154c2f4a5edf2658680e93d
Hristo Dimitrov <hristo.dimitrov@mentormate.com>
2023-12-18 19:12:35 +0200

    feat!: react native updated to 0.73.0

--- baseProject/ios/baseProject/AppDelegate.mm
+++ baseProject/ios/baseProject/AppDelegate.mm
@@ -16,6 +16,13 @@
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
+  return [self getBundleURL];
+}
+- (NSURL *)getBundleURL{
+  return [self getBundleURL];
+}
+- (NSURL *)getBundleURL
+{
 #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
 #else
